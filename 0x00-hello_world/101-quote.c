1c7ef9fc0629e0838ee4e50f4296f768a0c086d9
motunrayo1993 <omotunrayoegunjobi@gmail.com>
2022-04-07 15:26:52 -0700

    Task 8 added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
-#include include <stdio.h> 
- #include <unistd.h> 
- /** 
- * main - Entry point 
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main - Entry point
  *
- * Return: Always 0 (Success) 
- */ 
- int main(void) 
- { 
+ * Return: 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
- write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
- sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19")); 
- return (1); 
-
+	return (1);
 }
