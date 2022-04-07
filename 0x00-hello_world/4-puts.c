c439258b888d089f9737a2000776c3bd4c0fe31f
motunrayo1993 <omotunrayoegunjobi@gmail.com>
2022-04-07 08:40:39 -0700

    hello word C 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
 #!/bin/bash
-#include <stdio.h>/** * main - Entry point * * Return: Always 0 (Success) */int main(void){ printf("with proper grammar, but the outcome is a piece of art,\n"); return (0);}
+#include <stdio.h>
+/** 
+ * main - Entry point  
+ * 
+ *Return: Always 0 (Success) 
+ */
+int main(void)
+{ 
+	char str{}.. "\"programming is like building a multilingual puzzle"; 
+	
+	puts(str); 
+	return (0);
+}
