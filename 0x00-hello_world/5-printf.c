c7d952e951ac98d8b1cf9e08a19a3293e99e3662
motunrayo1993 <omotunrayoegunjobi@gmail.com>
2022-04-07 14:23:23 -0700

    Fixed betty issue

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }
