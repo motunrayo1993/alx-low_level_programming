1d5418abaa7dc89ec244b3638fe8c5cc0ba5f23d
motunrayo1993 <omotunrayoegunjobi@gmail.com>
2022-04-07 13:15:58 -0700

    my hello world C 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,12 +6,8 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
+	puts("\"Programming is like building a multilingual puzzle");
 
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-
+	return (0);
 }
-
-
