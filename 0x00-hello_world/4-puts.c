18e5e2aa670a96ef780569bdf95dce1bd7c56ce7
motunrayo1993 <omotunrayoegunjobi@gmail.com>
2022-04-07 09:32:03 -0700

    my hello world C 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,22 @@
- #include <stdio.h> 
+#include <stdio.h>
 
- /** 
-  *
-  *   * Description: main - Prints out a string to stdout. 
-  *
-  *     * Return: 0 if success. 
-  *
-  *       */ 
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+int main(void)
 
- int main(void) 
+{
 
-	 { 
+	printf("\" Programming is like building a multilingual puzzle ");
 
-		  puts("\"Programming is like building a multilingual puzzle"); 
+	return (0);
 
-		   return (0); 
-
-		    }
+}
 
 
