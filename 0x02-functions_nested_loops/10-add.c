c956b9568c143b0be8aeb26f10d0c3564c410e91
motunrayo1993 <99803657+motunrayo1993@users.noreply.github.com>
2022-04-13 23:33:16 +0100

    Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,23 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-void print_to_98(int n)
+*add - prints add two number
+*@i: print int n
+*@k: print in k
+*Return: Always 0.
+*/
+int add(int i, int k)
 {
-if (n < 98)
-{
-for (n = n; n < 98; n++)
-printf("%d, ", n);
-printf("%d\n", 98);
-}
-else
-{
-for (n = n; n > 98; n--)
-printf("%d, ", n);
-printf("%d\n", 98);
-}
+return (i + k);
 }
