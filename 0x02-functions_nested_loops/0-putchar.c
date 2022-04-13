4b3b9dbdaf7a641c391e8b46f5821125bbe91c4c
motunrayo1993 <99803657+motunrayo1993@users.noreply.github.com>
2022-04-13 23:00:55 +0100

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,20 +3,18 @@
 /**
  * main - Entry point
  *
- * Return: nothing, return void
+ * Return: nothing, return 0
  */
-
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
