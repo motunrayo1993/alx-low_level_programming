d9803a9d6ae6da2ae8a985a9a242dc04b7efcc01
motunrayo1993 <99803657+motunrayo1993@users.noreply.github.com>
2022-04-13 23:04:26 +0100

    Main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
-main.h
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -10,3 +11,8 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
