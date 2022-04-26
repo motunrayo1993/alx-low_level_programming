4f952c26a8fbf6368f403a604f9f203cfdcbe41c
motunrayo1993 <99803657+motunrayo1993@users.noreply.github.com>
2022-04-26 16:57:09 +0100

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+es (17 sloc)  278 Bytes
+   
+#include "main.h"
+
+/**
+ * _memcpy - prints buffer in hexa
+ * @src: buffer
+ * @dest: buffer2
+ * @n: size
+ * Return: Nothing.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
